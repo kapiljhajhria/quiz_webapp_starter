89cea298c95542c26f3f21bb3de986f358705cd0
kapil jhajhria <jhajhria44@gmail.com>
2020-04-16 10:11:55 +0530

    printing option text onclicking div

--- src/Option/index.js
+++ src/Option/index.js
@@ -3,9 +3,11 @@ import './styles.css';
 
 export default function Option(props) {
 
-
+    function printOption(){
+        console.log(`tapped on ${props.text}`);
+    }
     return (
-        <div
+        <div onClick={printOption}
             className={'option'}>
             {props.text}
         </div>
