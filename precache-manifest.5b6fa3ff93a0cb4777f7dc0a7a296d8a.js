02c9bedf5fc5102783fb3506b5ee419058d18def
kapil jhajhria <jhajhria44@gmail.com>
2020-04-16 13:30:39 +0530

    Updates

---
+++ precache-manifest.5b6fa3ff93a0cb4777f7dc0a7a296d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1cf474744055bf10f4ef7593b4ef46",
+    "url": "/quiz_webapp_starter/index.html"
+  },
+  {
+    "revision": "a1fda22f6e217867c029",
+    "url": "/quiz_webapp_starter/static/css/main.591df12d.chunk.css"
+  },
+  {
+    "revision": "7739fd6909ff6fd21f6b",
+    "url": "/quiz_webapp_starter/static/js/2.b22b6e85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz_webapp_starter/static/js/2.b22b6e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1fda22f6e217867c029",
+    "url": "/quiz_webapp_starter/static/js/main.530aab6a.chunk.js"
+  },
+  {
+    "revision": "c8de6f3d61835b00e455",
+    "url": "/quiz_webapp_starter/static/js/runtime-main.30231733.js"
+  }
+]);
