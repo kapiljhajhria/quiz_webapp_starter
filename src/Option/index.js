7a2d7b93b9b2718d6165f095706e0d462d1866d9
kapil jhajhria <jhajhria44@gmail.com>
2020-04-16 10:38:03 +0530

    changing color on click

--- src/Option/index.js
+++ src/Option/index.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles.css';
 
 export default function Option(props) {
-
+    const [selected, setSelected] = useState(false);
     function printOption(){
+        setSelected(!selected);
         console.log(`tapped on ${props.text}`);
     }
     return (
         <div onClick={printOption}
-            className={'option'}>
+            className={selected?"correct-option option":'option'}>
             {props.text}
         </div>
     )
