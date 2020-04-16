6c439f0bbcaf2dee56ff48c5ca1a87833b56e539
kapil jhajhria <jhajhria44@gmail.com>
2020-04-16 12:15:17 +0530

    wait for 3 seconds before moving to next question.

--- src/Quiz/index.js
+++ src/Quiz/index.js
@@ -8,30 +8,74 @@ class Quiz extends React.Component {
     state = {
         score: 0,
         selectedOption: null,
-        questionAnswered:false
+        questionAnswered: false,
+        currentQuestionIndex: 0,
 
     };
-    question = {
-        'text': "Who is current PM of India?",
-        'options': ['Narendra Modi', 'Rahul Gandhi', 'Manmohan Singh', 'Sonia Gandhi'],
-        'correct_choice': 0
-    };
+    questions = [
+        {
+            'text': "Who is current PM of India?",
+            'options': ['Narendra Modi', 'Rahul Gandhi', 'Manmohan Singh', 'Sonia Gandhi'],
+            'correct_choice': 0
+        },
+        {
+            'text': "What is capital of Orissa?",
+            'options': ['Chandigarh', 'Bhubaneshwar', 'Tripura', 'Nagaland'],
+            'correct_choice': 1
+        },
+        {
+            'text': "In the context to India's wild life, the flying fox is a __?",
+            'options': ['Bat', 'Vulture', 'Stok', 'Kite'],
+            'correct_choice': 0
+        },
+        {
+            'text': 'What percentage of land area should remain covered by forest to maintain Ecological balance?',
+            'options': ['10%', '33%', '5%', 'None of these'],
+            'correct_choice': 1
+        },
+        {
+            'text': ' The purest form of iron is',
+            'options': ['Wrought iron', 'Steel', 'Pig iron', 'Nickel steel'],
+            'correct_choice': 0
+        },
+        {
+            'text': 'Layer of atmosphere in which Ozone layer lies is',
+            'options': ['Exosphere', 'Mesosphere', 'Troposphere', 'Stratosphere'],
+            'correct_choice': 3
+        },
+    ];
+
+
     selectThis = (id) => {
-        if(!this.state.questionAnswered){
+        if (!this.state.questionAnswered) {
+            let copyCurrentQuestionIndex = this.state.currentQuestionIndex;
+            copyCurrentQuestionIndex = copyCurrentQuestionIndex + 1;
             console.log(`updating selectedOption to ${id}`);
-            let newScore=this.state.score;;
-            if(id===this.question.correct_choice){
-                newScore=newScore+10;
+            let newScore = this.state.score;
+
+            if (id === this.question.correct_choice) {
+                newScore = newScore + 10;
             }
             this.setState({
                 selectedOption: id,
                 score: newScore,
-                questionAnswered:true
+                questionAnswered: true,
             });
+
+            setTimeout(() => {
+                this.setState({
+                    selectedOption: null,
+                    questionAnswered: false,
+                    currentQuestionIndex: copyCurrentQuestionIndex,
+                });
+            }, 3000);
         }
     }
+    question;
 
     render() {
+        if (this.state.currentQuestionIndex < this.questions.length - 1)
+            this.question = this.questions[this.state.currentQuestionIndex];
         return (
             <div className="header">
                 <div className="score">Score: {this.state.score}</div>
