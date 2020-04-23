1c67c82ae2b64e4e2fc940aa3d7024ffc2dc11fd
kapil jhajhria <jhajhria44@gmail.com>
2020-04-23 20:12:39 +0530

    using map to render options

--- src/Quiz/index.js
+++ src/Quiz/index.js
@@ -140,18 +140,11 @@ class Quiz extends React.Component {
                 <div className="score">Score: {this.state.score}</div>
                 <Question questionText={this.question.text}/>
                 <div className="options-container">
-                    <Option queIndex={this.state.currentQuestionIndex} text={this.question.options[0]} id={0}
-                            isCorrect={this.question.correct_choice === 0}
-                            clicked={this.selectThis} selected={this.state.selectedOption === 0}/>
-                    <Option queIndex={this.state.currentQuestionIndex} text={this.question.options[1]} id={1}
-                            isCorrect={this.question.correct_choice === 1}
-                            clicked={this.selectThis} selected={this.state.selectedOption === 1}/>
-                    <Option queIndex={this.state.currentQuestionIndex} text={this.question.options[2]} id={2}
-                            isCorrect={this.question.correct_choice === 2}
-                            clicked={this.selectThis} selected={this.state.selectedOption === 2}/>
-                    <Option queIndex={this.state.currentQuestionIndex} text={this.question.options[3]} id={3}
-                            isCorrect={this.question.correct_choice === 3}
-                            clicked={this.selectThis} selected={this.state.selectedOption === 3}/>
+                    {this.question.options.map((option, idx) => (
+                        <Option queIndex={this.state.currentQuestionIndex} text={option} id={idx}
+                                isCorrect={this.question.correct_choice === idx}
+                                clicked={this.selectThis} selected={this.state.selectedOption === idx}/>
+                    ))}
                 </div>
                 <ProgressBar key={this.state.currentQuestionIndex} questionAnswered={this.state.questionAnswered}
                              moveToNextQuestion={() => this.moveToNextQuestion()}
